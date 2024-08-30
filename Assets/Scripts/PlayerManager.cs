114ac62c54be911e2b41c7ae2192709cac93dbb7
Joo-hyun Kim <jh1998@gne.go.kr>
2024-08-30 14:22:12 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using Cinemachine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private StarterAssetsInputs input;
+
+    [Header("Aim")]
+    [SerializeField]
+    private CinemachineVirtualCamera aimCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(input.aim)
+        {
+            aimCam.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            aimCam.gameObject.SetActive(false);
+        }
+    }
+}
